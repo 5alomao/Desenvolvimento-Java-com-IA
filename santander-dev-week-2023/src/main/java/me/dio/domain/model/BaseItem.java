7086f936f63ff1f6410e0d1b6d085fa20c1ae11b
unknown <salomao.junior2k22@gmail.com>
2024-07-21 18:50:13 -0300

    Implementação da camada de domínio.

---
+++ santander-dev-week-2023/src/main/java/me/dio/domain/model/BaseItem.java
@@ -0,0 +1,40 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class BaseItem {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String iconUrl;
+	private String description;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getIconUrl() {
+		return iconUrl;
+	}
+
+	public void setIconUrl(String iconUrl) {
+		this.iconUrl = iconUrl;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
